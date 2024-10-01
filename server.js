1d6945326882a92e687dc131e2a1fb3d488578f7
Piyush Raghav <156657568+Piyushraghav01@users.noreply.github.com>
2024-10-01 21:19:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-/* // Creating an http server
+/* // Creating an http server-1
 // Create a srever using NodeJS.
 
 // 1. Import http library/module
@@ -16,6 +16,34 @@ server.listen(3100, () => {
 })
 
 // console.log('Server is listening a port of 3100'); */
+
+/* // Handling Multiple Requests-2
+// Create a srever using NodeJS.
+
+// 1. Import http library/module
+const http = require('http');
+
+// 2. Create Server
+const server = http.createServer((req, res) => {
+    console.log(req.url);
+    res.write('Welcome to my Application');
+    if(req.url=="/product"){
+        // respone ends // using return keyword
+         res.end('This is Product Page');
+    }else if(req.url=="/user"){
+        res.end('This is User Page');
+    }
+    // Here comes the request.
+    res.end('Welcome to Node JS Server');
+});
+
+// 3. Specily a port to listen to client's request.
+server.listen(3100, () => {
+    console.log('Server is listening a port of 3100');
+})
+
+// console.log('Server is listening a port of 3100'); */
+
 
 
 
