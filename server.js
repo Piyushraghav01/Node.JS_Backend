15830f1b0c7cd1c915fc015e7294a9509c20ad72
Piyush Raghav <156657568+Piyushraghav01@users.noreply.github.com>
2024-10-01 21:36:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -44,6 +44,36 @@ server.listen(3100, () => {
 
 // console.log('Server is listening a port of 3100'); */
 
+/* // Returning HTML as Response-3
+const http = require('http');
+const fs = require('fs');
+
+
+const server = http.createServer((req, res) => {
+    const data = fs.readFileSync('index.html').toString()
+    res.end(data);
+});
+
+server.listen(3100);
+
+console.log('Server is listening on 3100');
+//Terminal node index
+//file index.html/index.js 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Welcome in Node JS</h1>
+    <p style="color: blue;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum dignissimos quo at ex minus incidunt qui asperiores ratione, rem hic.</p>
+</body>
+</html> */
+
+
+
 
 
 
